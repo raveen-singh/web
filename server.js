bd3b5983c9297ebea4ca3a7bfba711f015385237
Raveen Singh <emailraveensingh@gmail.com>
2020-06-29 19:39:38 -0400

    added env PORT

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ const automl = require('@google-cloud/automl');
 const bodyParser = require("body-parser");
 const fs = require('fs');
 const dotenv = require('dotenv');
+const path = require("path");
 dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 const projectId = 'usober-221401';
 const computeRegion = 'us-central1';
