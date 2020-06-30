c164d41184484a765bd52158b350ee5fc887738f
Raveen Singh <emailraveensingh@gmail.com>
2020-06-29 22:15:46 -0400

    enforced ssl

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const bodyParser = require("body-parser");
 const fs = require('fs');
 const dotenv = require('dotenv');
 const path = require("path");
+var sslRedirect = require('heroku-ssl-redirect');
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(sslRedirect());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
